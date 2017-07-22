e75e9d4b2affa20f08c7ae6321363b354556548a
Iku Iwasa <iku.iwasa@gmail.com>
2017-07-22 19:00:58 +0900

    Support Wiki External Filter plugin
    Copy the config file to redmine/config.

--- redmine/run-redmine.sh
+++ redmine/run-redmine.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+EXTERNAL_FILTER_CONFIG=/usr/src/redmine/plugins/wiki_external_filter/config/wiki_external_filter.yml
+
 bundle install --without development test
+
+if [ -f "$EXTERNAL_FILTER_CONFIG" ]; then
+	cp "$EXTERNAL_FILTER_CONFIG" /usr/src/redmine/config/
+fi
 
 exec /docker-entrypoint.sh rails server -b 0.0.0.0
